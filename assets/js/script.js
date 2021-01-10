efae968ba2776e7f4891dd1b82c199f43125f040
jbeard <player69>
2021-01-10 04:41:04 -0700

    continue work on js file for looping through questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,58 +3,77 @@ var mainEl = document.getElementById('main');
 var startBtn = document.getElementById('start');
 var questionTitle = document.getElementById('title');
 var questionAnswerList = document.getElementById('content');
+var questionSubmitted = false;
+
 
 // Set up the list of questions
 var questions = [
   {
-    question: "Question 1",
-    answers: {
-      a: "Answer A",
-      b: "Answer B",
-      c: "Answer C"
-    },
+    question: "Commonly used data types DO NOT include:",
+    answers: [
+      "1. strings",
+      "2. booleans",
+      "3. alerts",
+      "4. numbers"
+    ],
     correctAnswer: "c"
   },
   {
     question: "Question 2",
-    answers: {
-      a: "Answer A",
-      b: "Answer B",
-      c: "Answer C"
-    },
+    answers: [
+      "Answer A",
+      "Answer B",
+      "Answer C"
+    ],
     correctAnswer: "c"
   },
   {
     question: "Question 3",
-    answers: {
-      a: "Answer A",
-      b: "Answer B",
-      c: "Answer C",
-      d: "Answer D"
-    },
+    answers: [
+      "Answer A",
+      "Answer B",
+      "Answer C",
+      "Answer D"
+    ],
     correctAnswer: "d"
   },
   {
     question: "Question 4",
-    answers: {
-      a: "Answer A",
-      b: "Answer B",
-      c: "Answer C",
-      d: "Answer D"
-    },
+    answers: [
+      "Answer A",
+      "Answer B",
+      "Answer C",
+      "Answer D"
+    ],
     correctAnswer: "d"
   },
   {
     question: "Question 5",
-    answers: {
-      a: "Answer A",
-      b: "Answer B",
-      c: "Answer C",
-      d: "Answer D"
-    },
+    answers: [
+      "Answer A",
+      "Answer B",
+      "Answer C",
+      "Answer D"
+    ],
     correctAnswer: "d"
   }
 ];
+
+
+var formatQuestion = function (currentQuestion) {
+  questionTitle.textContent = questions[currentQuestion].question;
+  questionAnswerList.textContent = "";
+
+  for (i = 0; i < questions[currentQuestion].answers.length; i++) {
+    var answers = questions[currentQuestion].answers[i];
+    var button = document.createElement("button");
+    button.setAttribute("id", i);
+    button.setAttribute("onclick", "checkAnswer(this.id)");
+    button.textContent = answers;
+    questionAnswerList.appendChild(button);
+  }
+}
+
 
 // Hold variables for scoring
 var correctAnswer = 0;
@@ -65,20 +84,36 @@ var totalQuestions = questions.length;
 //Start the countdown when button pressed
 function countdown() {
   var timeLeft = 75;
+  
+  startBtn.textContent = "";
 
-  var timeInterval = setInterval(function() {
+  var timeInterval = setInterval(function () {
     if (timeLeft > 1) {
-      timerEl.innerHTML = "<b>Time: </b>" + timeLeft + " seconds remaining"; 
+      timerEl.innerHTML = "<b>Time: </b>" + timeLeft + " seconds remaining";
       timeLeft--;
-    } else if (timeLeft === 1){
+    } else if (timeLeft === 1) {
       timerEl.innerHTML = "<b>Time: </b>" + timeLeft + " second remaining";
       timeLeft--;
-    } else if(timeLeft === 0) {
+    } else if (timeLeft === 0) {
       timerEl.innerHTML = "<b>Time: </b>" + timeLeft + " seconds remaining";
       clearInterval(timeInterval);
     }
+    formatQuestion(questionsAnswered);
   }, 1000);
 }
 
 // Start timer
 startBtn.onclick = countdown;
+
+
+
+function checkAnswer(clicked_id,)
+{
+    alert(clicked_id);
+    questionsAnswered++;
+    formatQuestion(questionsAnswered)
+}
+
+
+
+
