9700d546bd83e5dc99bd1b122bc8d21ae4ce8cf4
jbeard <player69>
2021-01-10 00:20:03 -0700

    continue formatting and structing the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,68 @@
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
 var startBtn = document.getElementById('start');
+var questionTitle = document.getElementById('title');
+var questionAnswerList = document.getElementById('content');
 
+// Set up the list of questions
+var questions = [
+  {
+    question: "Question 1",
+    answers: {
+      a: "Answer A",
+      b: "Answer B",
+      c: "Answer C"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Question 2",
+    answers: {
+      a: "Answer A",
+      b: "Answer B",
+      c: "Answer C"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Question 3",
+    answers: {
+      a: "Answer A",
+      b: "Answer B",
+      c: "Answer C",
+      d: "Answer D"
+    },
+    correctAnswer: "d"
+  },
+  {
+    question: "Question 4",
+    answers: {
+      a: "Answer A",
+      b: "Answer B",
+      c: "Answer C",
+      d: "Answer D"
+    },
+    correctAnswer: "d"
+  },
+  {
+    question: "Question 5",
+    answers: {
+      a: "Answer A",
+      b: "Answer B",
+      c: "Answer C",
+      d: "Answer D"
+    },
+    correctAnswer: "d"
+  }
+];
+
+// Hold variables for scoring
+var correctAnswer = 0;
+var questionsAnswered = 0;
+var totalQuestions = questions.length;
+
+
+//Start the countdown when button pressed
 function countdown() {
   var timeLeft = 75;
 
@@ -19,4 +80,5 @@ function countdown() {
   }, 1000);
 }
 
+// Start timer
 startBtn.onclick = countdown;
